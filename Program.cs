07050afc5dba54aab9fe84f68b6d353c45c937e7
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-11-09 18:55:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Lesson11Timer;
+
+Whatcher whatcher = new Whatcher();
+Clock clock = new Clock();
+clock.timeTimer = 15;
+whatcher.AddClock(clock);
+clock.AddWhatcher(whatcher);
+whatcher.WhatcherEvent.Invoke(null, EventArgs.Empty);
---
+++ Timer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11Timer
+{
+    public sealed class Whatcher
+    {
+        public EventHandler WhatcherEvent;
+        public void AddClock(Clock clock)
+        {
+            clock.ClockEvent += ClockReceivedEvent;
+        }
+
+        private void ClockReceivedEvent(object? sender, EventArgs e)
+        {
+            WhatcherEvent.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+
+    public sealed class Clock
+    {
+        public EventHandler ClockEvent;
+        public int timeTimer;
+        public void AddWhatcher(Whatcher whatcher)
+        {
+            whatcher.WhatcherEvent += WhatcherReceivedEvent;
+        }
+
+        private void WhatcherReceivedEvent(object? sender, EventArgs e)
+        {
+            --timeTimer;
+            if (timeTimer == 0)
+            {
+                Console.WriteLine("Time is over");
+                return;
+            }
+            
+            Console.WriteLine($"Timer: {timeTimer}");
+            Thread.Sleep(1000);
+            Console.Clear();
+            ClockEvent.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+}
